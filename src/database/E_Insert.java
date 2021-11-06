b4d96cb187fd422e12cf5b24f2d8f9aaaa381517
vidigummy <vidigummy@gmail.com>
2021-11-06 18:52:08 +0900

    Insert 추가

--- src/database/E_Insert.java
+++ src/database/E_Insert.java
@@ -7,13 +7,14 @@ public class E_Insert extends Insert{
         Connector conn = new Connector();
         conn.connect();
         String query = String.format("INSERT INTO `EMPLOYEE` (`Fname`, `Minit`, `Lname`, `Ssn`, `Bdate`, `Address`, `Sex`, `Salary`, `Super_ssn`, `Dno`)" +
-                                                    " VALUES ('%s','%s','%s','%s','%s', '%s', '%c', '%s', '%s', '%d')",Fname,Minit,Lname,Ssn,Bdate,Address, Sex,Salary,Super_ssn,Dno);
+                                                    " VALUES ('%s','%s','%s','%s','%s', '%s', '%c', %s, %s, %d)",Fname,Minit,Lname,Ssn,Bdate,Address, Sex,Salary,Super_ssn,Dno);
         System.out.println(query);
-        boolean rs = conn.modify(query);
-        if(rs){
-            System.out.println("insert 성공");
-        }else{
-            System.out.println("insert 실패");
+        try {
+            boolean rs = conn.modify(query);
+            System.out.println("Insert 성공");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e);
         }
         conn.close();
     }
--- src/database/Main.java
+++ src/database/Main.java
@@ -17,7 +17,8 @@ public class Main {
 //        Sellect_filter Birthday = new Sellect_filter("생일","1965-01-09");
 //        //부하직원
 //        Sellect_filter Supvervising = new Sellect_filter("부하직원","Franklin T Wong")
-        E_Insert insert_test = new E_Insert("Vidi", "R", "Gummy", "125021", "1996-07-22", "SEOUL-GangDong", 'M', "100000.00", "Null", 1);
+//        Insert 테스트 코드 돈을 많이 버는 vidigummy를 만드는 방법 다른 방법 있으려나?
+        E_Insert insert_test = new E_Insert("Vidi", "R", "Gummy", "17125021", "1996-07-22", "SEOUL-GangDong", 'M', "100000.00", "NULL", 1);
 
 //  Delete Employee의 경우, 삭제를 하려면 객체를 가지고 있어야 한다.(당연쓰) So, Test Code를 Sellect_filter의 연봉 Select에 넣어놨으니 참고 바란다.
         // 원시적인 방법
